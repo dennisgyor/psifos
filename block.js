97ca5dcbad4758f5e90fc8718b31b3ab23d59cb6
Dennis Gyor <dennisgyor@gmail.com>
2021-02-23 07:46:33 -0600

    added crypto-js and created hash method

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+const SHA256 = require('crypto-js/sha256');
+
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -10,7 +13,7 @@ class Block {
         return `Block -
             Time Stamp : ${this.timestamp}
             Last Hash  : ${this.lastHash.substring(0, 10)}
-            This Hash  : ${this.hash.substring(0, 10)}
+            Hash       : ${this.hash.substring(0, 10)}
             Data       : ${this.data}`;
     }
     static genesis() {
@@ -20,10 +23,15 @@ class Block {
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'TODO-hash';
+        //calling static hash method and passing 3 params
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
     }
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp} ${lastHash} ${data}`).toString();
+    }
+    // Hash is generated using 3 params (timestamp, lasthash and data)
 }
 //export the module to be available for use in the project
 module.exports = Block;
